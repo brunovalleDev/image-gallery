50c8a408c03038eba0c8efcd683b814b544a84b3
Bruno Valle Silva <brunovallesilva93@gmail.com>
2023-05-27 08:07:38 -0300

    Melhora no código JS

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const botoesFiltros = document.querySelectorAll('.botoes-filtros button');
+const botoes = document.querySelectorAll('.botoes-filtros button');
 const cards = document.querySelectorAll('.cards__container .card');
 
-//adicionando evento de clique para cada botão
 
-botoesFiltros.forEach((botao) => {
-  botao.addEventListener("click", (element) => {
+botoes.forEach((botao) => {
+  botao.addEventListener('click', (element) => {
     document.querySelector('.active').classList.remove('active');
-    element.target.classList.add('active');
-
+    botao.classList.add('active');
+    
     cards.forEach((card) => {
       card.classList.add('hide');
-
-      if(card.dataset.name === element.target.dataset.name || element.target.dataset.name === "todos") {
+      
+      if(element.target.dataset.name === card.dataset.name || element.target.dataset.name === 'todos') {
         card.classList.remove('hide');
       }
     });
+ 
   });
+
+
 });
